1eceff0dad95cfbc92f3d6de669557ad6290139c
hoishin <hoishinxii@gmail.com>
2018-07-08 03:43:54 +0900

    directly require each function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-const modules = require('./build');
 
 exports.sgdq2018 = () => {
-	modules.sgdq2018.run();
+	require('./build/sgdq2018')();
 };
--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './sgdq2018';
