4453a4eb366a4ca87537baac39ec54a633baf59e
hoishin <hoishinxii@gmail.com>
2018-07-08 04:41:36 +0900

    import json for discord config

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,24 +1,22 @@
 import axios from 'axios';
-const {OUTPUT_WEBHOOK, SYSTEM_WEBHOOK} = process.env;
+import {Config} from '../types';
+
+const config: Config = require(`../../config/${process.env.SLS_STAGE}.json`);
+const discordConfig = config.discord;
 
 export class DiscordClient {
 	constructor(private readonly eventName: string) {}
 
 	public async output(content: string) {
-		if (!OUTPUT_WEBHOOK) {
-			throw new Error('Missing OUTPUT_WEBHOOK');
-		}
-		const webhookUrls = OUTPUT_WEBHOOK.split(',');
 		await Promise.all(
-			webhookUrls.map(url => this.post(url, this.eventName, content))
+			discordConfig.output.map(url =>
+				this.post(url, this.eventName, content)
+			)
 		);
 	}
 
 	public async system(content: string) {
-		if (!SYSTEM_WEBHOOK) {
-			throw new Error('Missing SYSTEM_WEBHOOK');
-		}
-		await this.post(SYSTEM_WEBHOOK, this.eventName, content);
+		await this.post(discordConfig.system, this.eventName, content);
 	}
 
 	private async post(url: string, username: string, content: string) {
