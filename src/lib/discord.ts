d48f1bcd3c6c6aab08d035b56aa53900a509c870
hoishin <hoishinxii@gmail.com>
2018-07-14 18:43:57 +0900

    format

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {Config} from '../types';
 
 const stage = process.env.SLS_STAGE || 'development';
+// tslint:disable:no-var-requires
 const config: Config = require(`../../config/${stage}.json`);
 const discordConfig = config.discord;
 
@@ -10,7 +11,7 @@ export class DiscordClient {
 
 	public async output(content: string) {
 		await Promise.all(
-			discordConfig.output.map(url =>
+			discordConfig.output.map(async url =>
 				this.post(url, this.eventName, content)
 			)
 		);
@@ -19,16 +20,16 @@ export class DiscordClient {
 	public async system(content: string) {
 		await this.post(
 			discordConfig.system,
-			this.eventName + ' ' + stage,
+			`${this.eventName} ${stage}`,
 			content
 		);
 	}
 
 	private async post(url: string, username: string, content: string) {
-		content = content.slice(0, 2000);
+		const slicedContent = content.slice(0, 2000);
 		const params = {
 			username,
-			content,
+			content: slicedContent,
 		};
 		await axios.post(url, params);
 	}
--- src/lib/dynamo.ts
+++ src/lib/dynamo.ts
@@ -1,13 +1,22 @@
 import {DynamoDB} from 'aws-sdk';
 
 const docClient = new DynamoDB.DocumentClient();
-const TABLE_NAME = process.env.TABLE_NAME!;
+const TABLE_NAME = process.env.TABLE_NAME;
 
 export class DynamoClient<T> {
+	private readonly TABLE_NAME: string;
+
+	constructor() {
+		if (!TABLE_NAME) {
+			throw new Error('Missing TABLE_NAME');
+		}
+		this.TABLE_NAME = TABLE_NAME;
+	}
+
 	async get(eventName: string): Promise<T | undefined> {
 		const res = await docClient
 			.get({
-				TableName: TABLE_NAME,
+				TableName: this.TABLE_NAME,
 				Key: {eventName},
 			})
 			.promise();
@@ -17,7 +26,7 @@ export class DynamoClient<T> {
 	async put(eventName: string, data: T) {
 		await docClient
 			.put({
-				TableName: TABLE_NAME,
+				TableName: this.TABLE_NAME,
 				Item: {
 					eventName,
 					data: JSON.stringify(data),
